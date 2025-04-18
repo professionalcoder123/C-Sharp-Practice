5f9ae6e3749e498800a871bc3cb7e90af3b172bf
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 11:45:20 +0530

    Checking whether the number entered is positive, negative or zero

---
+++ BasicPrograms/BasicPrograms/NumberIdentification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class NumberIdentification
+    {
+        public static string CheckNumber()
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            if (num < 0)
+            {
+                return "Negative number";
+            }
+            else if (num > 0)
+            {
+                return "Positive number";
+            }
+            else
+            {
+                return "Zero";
+            }
+        }
+    }
+}
