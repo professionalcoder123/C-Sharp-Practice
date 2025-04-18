1eadb6e58bc2b9a29684b7660fdb5a5ddfb5431f
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 10:13:02 +0530

    Second commit

---
+++ BasicPrograms/BasicPrograms/NumberSubtraction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class NumberSubtraction
+    {
+        public static void SubtractNumbers()
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = num1 - num2;
+            Console.WriteLine("Subtraction of numbers is " + num3);
+        }
+    }
+}
--- BasicPrograms/BasicPrograms/Program.cs
+++ BasicPrograms/BasicPrograms/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             NumberAddition.AddNumbers();
+
+            NumberSubtraction.SubtractNumbers();
         }
     }
 }
