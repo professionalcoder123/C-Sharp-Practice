37e296fc737031cbfbf4132d4d49afd95d62667e
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 17:23:00 +0530

    Counted the number of digits in a given number

---
+++ BasicPrograms/BasicPrograms/NumberOfDigits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class NumberOfDigits
+    {
+        public static void PrintNumberOfDigits()
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int count = 0;
+            while (num != 0)
+            {
+                int digit = num % 10;
+                num = num / 10;
+                count++;
+            }
+            Console.WriteLine("Number of digits in a number are " + count);
+        }
+    }
+}
