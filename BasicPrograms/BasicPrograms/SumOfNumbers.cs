98182fe3bab199ab3c6bf728182f2467ce09f694
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 17:25:30 +0530

    Calculated sum of first 'n' natural numbers based on the value of 'n'

---
+++ BasicPrograms/BasicPrograms/SumOfNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class SumOfNumbers
+    {
+        public static void PrintSum()
+        {
+            Console.WriteLine("Enter the value of 'n' upto which you want the sum");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Sum of first 'N' natural numbers is " + sum);
+        }
+    }
+}
