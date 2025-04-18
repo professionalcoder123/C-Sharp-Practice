44ccd043702eb249016c147b60dc5ab140951eae
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 14:49:32 +0530

    Printing all even numbers from 1 to 100

---
+++ BasicPrograms/BasicPrograms/EvenNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class EvenNumbers
+    {
+        public static void DisplayEvenNumbers()
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
