8f1a00f06c1b767dc22a243848b9cacad0ae55c8
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 13:29:23 +0530

    Checking whether the number is divisible by 5 and 11 or not

---
+++ BasicPrograms/BasicPrograms/NumberDivisibility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class NumberDivisibility
+    {
+        public static bool CheckDivisibility()
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 5 == 0 && num % 11 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
