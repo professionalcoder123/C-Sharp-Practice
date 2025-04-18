8763956e11e360e859a978bcf4764a794e01014b
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 10:45:14 +0530

    Finding the maximum number between the two numbers

---
+++ BasicPrograms/BasicPrograms/MaximumNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class MaximumNumber
+    {
+        public static void FindMaximum()
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("Two numbers are equal. Cannot find greater number!");
+            }
+            else
+            {
+                if (num1 > num2)
+                {
+                    Console.WriteLine("First number is greater");
+                }
+                else
+                {
+                    Console.WriteLine("Second number is greater");
+                }
+            }
+        }
+    }
+}
