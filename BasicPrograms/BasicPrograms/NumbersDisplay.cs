20b31b0253ab229f05ae74fcade5b5149c0e2fbe
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 14:43:34 +0530

    Printing numbers from 1 to 100

---
+++ BasicPrograms/BasicPrograms/NumbersDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class NumbersDisplay
+    {
+        public static void PrintNumbers()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
