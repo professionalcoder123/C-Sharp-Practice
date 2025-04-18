e403989ba9b625d1714657cbb820b0388a0f2361
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 13:40:55 +0530

    Checking whether an entered year is a leap year or not

---
+++ BasicPrograms/BasicPrograms/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class LeapYear
+    {
+        public static void CheckLeapYear()
+        {
+            Console.WriteLine("Enter a year");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                Console.WriteLine("Entered year is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("Entered year is not a leap year");
+            }
+        }
+    }
+}
