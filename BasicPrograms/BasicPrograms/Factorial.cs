4a807997b79b61055b049faa6bcea104ed027b7c
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 17:21:05 +0530

    Calculated factorial of a number

---
+++ BasicPrograms/BasicPrograms/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class Factorial
+    {
+        public static int FindFactorial()
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int fact = 1;
+            for(int i = num; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+}
