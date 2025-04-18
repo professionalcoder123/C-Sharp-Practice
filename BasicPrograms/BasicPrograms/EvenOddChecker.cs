bbe3873941c93092b555ce667f2f9dfe8d4df7ca
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 10:35:10 +0530

    Checking whether the number is even or odd

---
+++ BasicPrograms/BasicPrograms/EvenOddChecker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class EvenOddChecker
+    {
+        public static string CheckEvenOrOdd()
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                return "Even number";
+            }
+            else
+            {
+                return "Odd number";
+            }
+        }
+    }
+}
