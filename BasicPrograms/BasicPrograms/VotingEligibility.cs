eca484e21de24b527785ca46340a9cdc65deff42
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 12:51:48 +0530

    Checking whether a person has an eligibilty to vote or not

---
+++ BasicPrograms/BasicPrograms/VotingEligibility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class VotingEligibility
+    {
+        public static void CheckEligibility()
+        {
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            if (age < 18)
+            {
+                Console.WriteLine("You are not eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("You are eligible to vote");
+            }
+        }
+    }
+}
