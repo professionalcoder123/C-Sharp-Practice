76a3f4e5f76d971ed555a740e6ed80694eacd870
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 13:17:30 +0530

    Checking whether an entered character is a vowel or a consonant

---
+++ BasicPrograms/BasicPrograms/CharacterCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class CharacterCheck
+    {
+        public static string CheckCharacter()
+        {
+            Console.WriteLine("Enter a character");
+            char ch = Console.ReadKey().KeyChar;
+            Console.ReadLine();
+            if ((ch == 'a' || ch == 'A') || (ch == 'e' || ch == 'E') || (ch == 'i' || ch == 'I') || (ch == 'o' || ch == 'O') || (ch == 'u' || ch == 'U'))
+            {
+                return "Vowel";
+            }
+            else
+            {
+                return "Consonant";
+            }
+        }
+    }
+}
