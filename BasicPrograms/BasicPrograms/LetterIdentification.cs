1b491afc5d3a14e5d8e90834bbec8dd464b797db
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-18 13:51:41 +0530

    Checking whether an entered key is an alphabet, number or a special character

---
+++ BasicPrograms/BasicPrograms/LetterIdentification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class LetterIdentification
+    {
+        public static string CheckLetter()
+        {
+            Console.WriteLine("Enter a character");
+            char ch = Console.ReadKey().KeyChar;
+            Console.ReadLine();
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                return "Alphabet";
+            }
+            else if (ch >= '0' && ch <= '9')
+            {
+                return "Digit";
+            }
+            else
+            {
+                return "Special character";
+            }
+        }
+    }
+}
