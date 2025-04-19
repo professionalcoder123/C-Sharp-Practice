e124f34c0c2c7331c7e03ee583ce0d69d96f6b69
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-19 22:12:59 +0530

    Finding the longest word in a given string by converting it into an array of strings

---
+++ BasicPrograms/BasicPrograms/LongestWord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class LongestWord
+    {
+        public static void FindLongestWord()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            string[] strArray = str.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string longestWord = "";
+            foreach(string word in strArray)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+            Console.WriteLine("Longest string in a given string is " + "\'" + longestWord + "\'");
+        }
+    }
+}
