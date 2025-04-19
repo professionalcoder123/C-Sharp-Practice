d146d9a2c321efc6379dde9c7df9a930fb54f2d6
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-19 23:10:30 +0530

    Printing only strings from an array list

---
+++ Non-GenericCollections/Non-GenericCollections/StringsDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_GenericCollections
+{
+    internal class StringsDisplay
+    {
+        public static void DisplayStrings()
+        {
+            ArrayList al = new ArrayList();
+            al.Add(10);
+            al.Add("Aditya");
+            al.Add(true);
+            al.Add(20);
+            al.Add("Sohel");
+            al.Add(false);
+            for(int i = 0; i < al.Count; i++)
+            {
+                string str = al[i] as string;
+                if (str != null)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
