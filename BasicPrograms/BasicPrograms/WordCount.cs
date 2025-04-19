40dbe027aeebebb22693fd265fd37e66ffda2a38
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-19 22:17:04 +0530

    Counting the total number of words in an input string

---
+++ BasicPrograms/BasicPrograms/WordCount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class WordCount
+    {
+        public static void CountWords()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            string[] strArr = str.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            int count = strArr.Length;
+            Console.WriteLine("Total number of words in a string are " + count);
+        }
+    }
+}
