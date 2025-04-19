a059e66c7406a423934d743c441c2cfc26b81f08
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-19 19:04:47 +0530

    Printing the same string but without duplicates

---
+++ BasicPrograms/BasicPrograms/DuplicateCharacters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class DuplicateCharacters
+    {
+        public static void RemoveDuplicates()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            str = str.ToLower();
+            string result = "";
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            Console.Write("Resultant string without duplicates : ");
+            foreach(char ch in str)
+            {
+                if (!dict.ContainsKey(ch))
+                {
+                    dict[ch] = 1;
+                    result = result + ch;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
