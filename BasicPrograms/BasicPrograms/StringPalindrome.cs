83051d436c02a9d604d6f1c05c3a2e6ef6047f45
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-19 16:29:04 +0530

    Checking whether if an entered string is a palindrome or not

---
+++ BasicPrograms/BasicPrograms/StringPalindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class StringPalindrome
+    {
+        public static bool IsPalindrome()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            str = str.ToLower();
+            string copy = str;
+            string rev = "";
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                rev = rev + str[i];
+            }
+            if (copy == rev)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
