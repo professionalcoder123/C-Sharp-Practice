a728b602de46c4ba38f0d8c73161c857e06f4c6c
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-19 22:10:45 +0530

    Checking the frequency of each character in a string

---
+++ BasicPrograms/BasicPrograms/CharacterFrequency.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class CharacterFrequency
+    {
+        public static void FindFrequency()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            str = str.ToLower();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            foreach(char ch in str)
+            {
+                if (!dict.ContainsKey(ch))
+                {
+                    dict[ch] = 1;
+                }
+                else
+                {
+                    dict[ch]++;
+                }
+            }
+            foreach(var pair in dict)
+            {
+                Console.WriteLine(pair.Key + " : " + pair.Value);
+            }
+        }
+    }
+}
