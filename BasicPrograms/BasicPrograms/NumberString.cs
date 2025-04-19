2f76b930260057886aa4f2d0b231eef7f0dff57f
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-19 22:13:48 +0530

    Checking if the strings has only digits in it or not

---
+++ BasicPrograms/BasicPrograms/NumberString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class NumberString
+    {
+        public static void CheckDigitsInString()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            int temp = 0;
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (!char.IsDigit(str[i]))
+                {
+                    temp++;
+                    break;
+                }
+            }
+            if (temp>0)
+            {
+                Console.WriteLine("Invalid string");
+            }
+            else
+            {
+                Console.WriteLine("Valid string");
+            }
+        }
+    }
+}
