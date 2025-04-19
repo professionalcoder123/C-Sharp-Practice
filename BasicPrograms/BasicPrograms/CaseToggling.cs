8797fdf2332df7fcd3e714b16f01d3e88e3eb234
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-19 22:09:51 +0530

    Converting the character case of string to its opposite case

---
+++ BasicPrograms/BasicPrograms/CaseToggling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class CaseToggling
+    {
+        public static string ToggleCase()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            string result = "";
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLower(str[i]))
+                {
+                    result += char.ToUpper(str[i]);
+                }
+                else
+                {
+                    result += char.ToLower(str[i]);
+                }
+            }
+            return result;
+        }
+    }
+}
