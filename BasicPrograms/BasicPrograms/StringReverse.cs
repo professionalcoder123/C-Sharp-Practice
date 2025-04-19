9d09dfa75057f63b93bf1734215052f014bf517e
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-19 22:14:43 +0530

    Finding the reverse of an input string

---
+++ BasicPrograms/BasicPrograms/StringReverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class StringReverse
+    {
+        public static string ReverseString()
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            str = str.ToLower();
+            string rev = "";
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                rev = rev + str[i];
+            }
+            return rev;
+        }
+    }
+}
