3d0598486d83fb14a571bf554726846df4aac7a6
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 14:01:29 +0530

    Handled the exception related to index which is outside the bounds of an array

---
+++ ExceptionHandling/ExceptionHandling/IndexAccessing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class IndexAccessing
+    {
+        public static void HandleIndexException()
+        {
+            try
+            {
+                int[] arr = new int[] { 1, 2, 3, 4, 5 };
+                Console.WriteLine(arr[10]);
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed successfully!");
+            }
+        }
+    }
+}
