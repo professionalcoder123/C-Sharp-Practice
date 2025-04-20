4c9c7b7a3bea9e8b61667059c6add8666651e246
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 14:02:37 +0530

    Handling multiple exceptions related to input and division of numbers

---
+++ ExceptionHandling/ExceptionHandling/InputAndDivisionException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class InputAndDivisionException
+    {
+        public static void HandleExceptions()
+        {
+            try
+            {
+                Console.WriteLine("Enter first number");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number");
+                int num2 = int.Parse(Console.ReadLine());
+                double res = num1 / num2;
+                Console.WriteLine("Division of numbers is "+res);
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine(fe);
+            }
+            catch(DivideByZeroException de)
+            {
+                Console.WriteLine(de);
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed successfully!");
+            }
+        }
+    }
+}
