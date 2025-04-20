a9c27e31cd3b45c05b0bea7bfab711715e3cc9bc
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 14:00:23 +0530

    Handled the exception related to age as input

---
+++ ExceptionHandling/ExceptionHandling/AgeFormat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class AgeFormat
+    {
+        public static void HandleFormatException()
+        {
+            try
+            {
+                Console.WriteLine("Enter an age");
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Age of a person is " + age);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed successfully!");
+            }
+        }
+    }
+}
