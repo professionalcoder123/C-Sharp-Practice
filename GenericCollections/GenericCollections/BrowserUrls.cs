041ab6bfad9d459522e17c8ed9f07f87832c47ec
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 11:24:48 +0530

    Pushing the URLs into the stack and displaying the URL history after popping one-by-one

---
+++ GenericCollections/GenericCollections/BrowserUrls.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    internal class BrowserUrls
+    {
+        public static void DisplayBrowserUrls()
+        {
+            Stack<string> urls = new Stack<string>();
+            Console.WriteLine("Enter the number of URLs you want");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the URL");
+                string url = Console.ReadLine();
+                urls.Push(url);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Popped URLs :");
+            for(int i = 0; i < n; i++)
+            {
+                string url = urls.Pop();
+                Console.WriteLine(url);
+            } 
+        }
+    }
+}
