cf20385477fa6de9132d440f7dbfe8e4784f80a1
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 08:22:54 +0530

    Enqueued the elements and displayed the dequeued elements one by one

---
+++ Non-GenericCollections/Non-GenericCollections/QueueOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_GenericCollections
+{
+    internal class QueueOperations
+    {
+        public static void PerformOperations()
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue(10);
+            queue.Enqueue(12.5f);
+            queue.Enqueue(32.9);
+            queue.Enqueue("Aditya");
+            queue.Enqueue(true);
+
+            int count = queue.Count;
+            Console.WriteLine("Dequeued elements :");
+            for(int i = 0; i < count; i++)
+            {
+                var element = queue.Dequeue();
+                Console.Write(element+" ");
+            }
+        }
+    }
+}
