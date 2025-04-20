bea30c711a3b0fc60f45b5a7f3bc5771da141cf5
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 11:39:21 +0530

    Enqueuing the elements in the queue and displaying them while dequeing one-by-one

---
+++ GenericCollections/GenericCollections/QueueOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    internal class QueueOperations
+    {
+        public static void PerformOperations()
+        {
+            Queue<int> queue = new Queue<int>();
+            int count = 10;
+            for(int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Enter an element");
+                int element = int.Parse(Console.ReadLine());
+                queue.Enqueue(element);
+            }
+            for(int i = 0; i < 3; i++)
+            {
+                queue.Dequeue();
+            }
+            Console.WriteLine("Remaining elements :");
+            foreach(int number in queue)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
