fbf60bf1ba59b43e5c01126630d944989e21c4be
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 14:09:36 +0530

    Implemented exception handling in this class

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberDivision.HandleDivisionException();
+
+            AgeFormat.HandleFormatException();
+
+            IndexAccessing.HandleIndexException();
+
+            NullString.HandleNullReferenceException();
+
+            InputAndDivisionException.HandleExceptions();
+
+            InvalidAge.HandleException();
+
+            PasswordCheck.HandlePasswordException();
+
+            StudentMarksValidation.HandleMarksOutOfBoundsException();
+        }
+    }
+}
