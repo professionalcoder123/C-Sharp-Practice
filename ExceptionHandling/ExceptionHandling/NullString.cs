9e69633223935dd35c221564f98d147754d80bc2
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 14:05:25 +0530

    Handled the custom exception related to printing the null string length

---
+++ ExceptionHandling/ExceptionHandling/NullString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class NullString
+    {
+        public static void HandleNullReferenceException()
+        {
+            try
+            {
+                string str = null;
+                Console.WriteLine(str.Length);
+            }
+            catch(NullReferenceException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed successfully!");
+            }
+        }
+    }
+}
