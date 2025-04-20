19dc8f8bb8e0d35b9dbb01b2fdf752437c868d00
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 22:46:35 +0530

    Implemented the thread execution delay in some milliseconds in all three functions

---
+++ Multithreading/Multithreading/ThreadsDelayDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class ThreadsDelayDemo
+    {
+        public static void PrintNumbers()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Number : {i}");
+                Thread.Sleep(3000);
+            }
+        }
+
+        public static void PrintLetters()
+        {
+            for(char i='A'; i <= 'Z'; i++)
+            {
+                Console.WriteLine($"Letter : {i}");
+                Thread.Sleep(2000);
+            }
+        }
+
+        public static void PrintMessage(object message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
