1d3eacee5c09e9ca25a32aac7555b59ba6aaf066
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 14:03:48 +0530

    Handling the custom exception related to age less than 18

---
+++ ExceptionHandling/ExceptionHandling/InvalidAge.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(string message) : base(message) { }
+    }
+
+    internal class InvalidAge
+    {
+        public static void HandleException()
+        {
+            try
+            {
+                Console.WriteLine("Enter the age");
+                int age = int.Parse(Console.ReadLine());
+                if (age < 18)
+                {
+                    throw new InvalidAgeException("Cannot input age less than 18!");
+                }
+                else
+                {
+                    Console.WriteLine("Age is valid!");
+                }
+            }
+            catch(InvalidAgeException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed successfully!");
+            }
+        }
+    }
+}
