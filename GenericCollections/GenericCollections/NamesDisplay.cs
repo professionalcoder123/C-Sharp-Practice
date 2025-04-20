c3d8041ade0cd7705ece401248769e706397f765
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 11:36:51 +0530

    Printing the strings which start with 'A'

---
+++ GenericCollections/GenericCollections/NamesDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    internal class NamesDisplay
+    {
+        public static void PrintNames()
+        {
+            List<string> nameList = new List<string>();
+            Console.WriteLine("Enter the number of elements to be added in the list");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter element " + (i + 1));
+                string name = Console.ReadLine();
+                nameList.Add(name);
+            }
+            var names = nameList.Where(n => n.StartsWith('A')).ToList();
+            Console.WriteLine("Names starting with 'A' :");
+            foreach(string name in names)
+            {
+                Console.Write(name + " ");
+            }
+        }
+    }
+}
