c50514b7bd8ea1af66e42f2eb0cda84e75e7d640
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 11:44:19 +0530

    Displaying the cities without their duplicates

---
+++ GenericCollections/GenericCollections/UniqueCities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    internal class UniqueCities
+    {
+        public static void PrintCities()
+        {
+            HashSet<string> cities = new HashSet<string>();
+            Console.WriteLine("Enter the number of cities");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the city");
+                string city = Console.ReadLine();
+                if (!cities.Contains(city))
+                {
+                    cities.Add(city);
+                }
+            }
+            Console.WriteLine("Unique cities :");
+            foreach(string city in cities)
+            {
+                Console.Write(city + " ");
+            }
+        }
+    }
+}
