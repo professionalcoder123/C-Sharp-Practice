d2b1251cd8639d6559c37c104aaef6f9b90ef3d4
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 11:47:44 +0530

    Implemented generic collections along with Lambda Expressions and LINQs in this class

---
+++ GenericCollections/GenericCollections/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenNumbers.DisplayEvenNumbers();
+
+            NamesDisplay.PrintNames();
+
+            StudentDetails.SortStudentsByName();
+
+            BrowserUrls.DisplayBrowserUrls();
+
+            QueueOperations.PerformOperations();
+
+            UniqueCities.PrintCities();
+
+            EmployeeDetails.PrintEmployeeWithHighSalary();
+
+            ProductDetails.SortProductsById();
+
+            StudentSorting.SortStudents();
+
+            LongestString.FindLongestString();
+        }
+    }
+}
