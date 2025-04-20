47f870efcd02d98c9468557562b139fda39be5ec
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 08:24:32 +0530

    Displayed the roll number and name of student in ascending order by roll number using sorted list

---
+++ Non-GenericCollections/Non-GenericCollections/SortedListOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_GenericCollections
+{
+    internal class SortedListOperations
+    {
+        public static void SortByRollNumber()
+        {
+            SortedList list = new SortedList();
+            list.Add(10, "Aditya");
+            list.Add(84, "Priyanka");
+            list.Add(73, "Sohel");
+            list.Add(30, "Kedar");
+            list.Add(45, "Kalpesh");
+            foreach(DictionaryEntry entry in list)
+            {
+                Console.Write($"Roll No. : {entry.Key}, Name : {entry.Value}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
