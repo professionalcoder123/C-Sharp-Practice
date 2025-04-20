0751bd5d736d11a766ec848e85848716b1bbdb3d
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 07:46:36 +0530

    Performed the operations of pushing and popping the elements and displaying popped elements

---
+++ Non-GenericCollections/Non-GenericCollections/StackOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_GenericCollections
+{
+    internal class StackOperations
+    {
+        public static void PerformOperations()
+        {
+            Stack stack = new Stack();
+
+            stack.Push(10);
+            stack.Push(12.6);
+            stack.Push(12.5f);
+            stack.Push("Aditya");
+            stack.Push(true);
+
+            int count = stack.Count;
+            Console.WriteLine("Popped elements :");
+            for(int i = 0; i < count; i++)
+            {
+                var element = stack.Pop();
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
