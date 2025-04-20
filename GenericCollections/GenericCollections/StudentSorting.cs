f2d83294d3fe58c5a89dbb60f06a2c1f13f0ed3d
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 11:42:12 +0530

    Sorted the student details by name

---
+++ GenericCollections/GenericCollections/StudentSorting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    internal class StudentNames
+    {
+        public static void SortStudents()
+        {
+            SortedList<string, int> students = new SortedList<string, int>();
+            Console.WriteLine("Enter the number of key-value pairs to be added");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the name of a student");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter the marks of a student");
+                int marks = int.Parse(Console.ReadLine());
+                students.Add(name, marks);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sorted students :");
+            foreach(var student in students)
+            {
+                Console.Write($"Student Name : {student.Key}, Marks : {student.Value}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
