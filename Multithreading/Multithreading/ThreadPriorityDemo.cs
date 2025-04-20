3480193e116d1d430e68c142a2a9021b476698ba
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 22:41:38 +0530

    Implemented the thread priority-based multithreading by assigning lowest, highest and normal priorities to threads

---
+++ Multithreading/Multithreading/ThreadPriorityDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class ThreadPriorityDemo
+    {
+        public static void PrintA()
+        {
+            for(int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine("A = " + i);
+            }
+        }
+
+        public static void PrintB()
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine("B = " + i);
+            }
+        }
+
+        public static void PrintC()
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine("C = " + i);
+            }
+        }
+    }
+}
