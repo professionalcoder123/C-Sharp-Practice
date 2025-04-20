0520f63c9846589c5a9f7ce8b28214f113a82170
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 11:40:23 +0530

    Displaying the student details sorted by their name

---
+++ GenericCollections/GenericCollections/StudentDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    internal class StudentDetails
+    {
+        public static void SortStudentsByName()
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            Console.WriteLine("Enter the number of key-value pairs to be added");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the roll number of student");
+                int rollNo = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the name of the student");
+                string name = Console.ReadLine();
+                students.Add(rollNo, name);
+            }
+            var sortedStudents = students.OrderBy(d => d.Value);
+            Console.WriteLine("Students sorted by names :");
+            foreach(var student in sortedStudents)
+            {
+                Console.Write($"Roll No. : {student.Key}, Name : {student.Value}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
