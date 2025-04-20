acd0fd86b6cea601268b98abe9c21a6eab2aba8b
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 22:48:51 +0530

    Implemented multithreading concepts inside this class

---
+++ Multithreading/Multithreading/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace Multithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread t1 = new Thread(ThreadsDemo.Func1);
+            //Thread t2 = new Thread(ThreadsDemo.Func2);
+            //Thread t3 = new Thread(ThreadsDemo.Func3);
+            //t1.Start();
+            //t2.Start();
+            //t3.Start();
+
+            //Thread t1 = new Thread(ThreadsDelayDemo.PrintNumbers);
+            //Thread t2 = new Thread(ThreadsDelayDemo.PrintLetters);
+            //Thread t3 = new Thread(ThreadsDelayDemo.PrintMessage);
+            //t1.Start();
+            //t2.Start();
+            //t3.Start("This is a message returning function");
+            //t1.Join();
+            //t2.Join();
+            //t3.Join();
+
+            //Thread t1 = new Thread(ThreadPriorityDemo.PrintA);
+            //Thread t2 = new Thread(ThreadPriorityDemo.PrintB);
+            //Thread t3 = new Thread(ThreadPriorityDemo.PrintC);
+            //t1.Priority = ThreadPriority.Highest;
+            //t2.Priority = ThreadPriority.Normal;
+            //t3.Priority = ThreadPriority.Lowest;
+            //t1.Start();
+            //t2.Start();
+            //t3.Start();
+
+            //Process currentProcess = Process.GetCurrentProcess();
+            //Console.WriteLine($"Process ID : {currentProcess.Id}");
+            //Console.WriteLine($"Process Name : {currentProcess.ProcessName}");
+            //Thread t1 = new Thread(ThreadProcessDemo.PrintNumbers);
+            //Thread t2 = new Thread(ThreadProcessDemo.PrintLetters);
+            //t1.Start();
+            //t2.Start();
+            //t1.Join();
+            //t2.Join();
+
+            //Thread t1 = new Thread(DeadlockDemo.Thread1);
+            //t1.Start();
+            //Thread t2 = new Thread(DeadlockDemo.Thread2);
+            //t2.Start();
+        }
+    }
+}
