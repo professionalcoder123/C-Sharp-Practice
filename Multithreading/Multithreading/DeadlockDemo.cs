08845ff6409d0736fcbbf33c1c7902cc00b784fb
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 22:38:58 +0530

    Created three threads which are accessing the three methods

---
+++ Multithreading/Multithreading/DeadlockDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class DeadlockDemo
+    {
+        static object lockA = new object();
+        static object lockB = new object();
+        
+        public static void Thread1()
+        {
+            lock (lockA)
+            {
+                Console.WriteLine("Thread1 locked A");
+                Thread.Sleep(1000);
+                lock (lockB)
+                {
+                    Console.WriteLine("Thread1 locked B");
+                }
+            }
+        }
+
+        public static void Thread2()
+        {
+            lock (lockB)
+            {
+                Console.WriteLine("Thread2 locked B");
+                Thread.Sleep(1000);
+                lock (lockA)
+                {
+                    Console.WriteLine("Thread2 locked A");
+                }
+            }
+        }
+    }
+}
