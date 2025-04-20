b7eccab2e019eca12bee0a292fdc4ab6102da374
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 07:28:30 +0530

    Updated the class with first method

---
+++ Non-GenericCollections/Non-GenericCollections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Non_GenericCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringsDisplay.DisplayStrings();
+        }
+    }
+}
