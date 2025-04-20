25ee17d13c45ccca535608e9c642dc02172034e4
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-20 22:44:57 +0530

    Implemented the live demonstration of process with the help of three threads

---
+++ Multithreading/Multithreading/ThreadProcessDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class ThreadProcessDemo
+    {
+        public static void PrintNumbers()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Number : {i}");
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void PrintLetters()
+        {
+            for(char i = 'A'; i <= 'Z'; i++)
+            {
+                Console.WriteLine($"Letter : {i}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
