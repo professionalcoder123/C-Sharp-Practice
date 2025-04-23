d8332e2b443b61a0d6ce835cdd3b8ad34788a72e
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-23 22:03:19 +0530

    Demonstrated the concurrent execution of threads in parallel

---
+++ Multithreading/Multithreading/ThreadPoolDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class ThreadPoolDemo
+    {
+        public static void WorkItem1(object state)
+        {
+            Console.WriteLine($"WorkItem1 : {state}");
+        }
+
+        public static void WorkItem2(object state)
+        {
+            int num = (int)state;
+            int square = num * num;
+            Console.WriteLine($"WorkItem2 : {square}");
+        }
+    }
+}
