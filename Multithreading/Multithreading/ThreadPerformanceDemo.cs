831b8cc561a0cd99ebe1c77054e2be8937706b2e
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-23 22:05:08 +0530

    Checked the difference in the execution of two threads in milliseconds in single-threaded model and multi-threaded model

---
+++ Multithreading/Multithreading/ThreadPerformanceDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class ThreadPerformanceDemo
+    {
+        public static void IncrementCounter1()
+        {
+            long count = 0;
+            for(int i = 1; i <= 1000000000; i++)
+            {
+                count++;
+            }
+            Console.WriteLine($"IncrementCounter1 : {count}");
+        }
+
+        public static void IncrementCounter2()
+        {
+            long count = 0;
+            for (int i = 1; i <= 1000000000; i++)
+            {
+                count++;
+            }
+            Console.WriteLine($"IncrementCounter2 : {count}");
+        }
+    }
+}
