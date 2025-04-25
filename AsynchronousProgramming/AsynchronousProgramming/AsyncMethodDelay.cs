a72240ad7d4e27fad733d84040a91154d6d2598a
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 16:08:58 +0530

    Demonstrated the execution of three different methods with a different delay

---
+++ AsynchronousProgramming/AsynchronousProgramming/AsyncMethodDelay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal class AsyncMethodDelay
+    {
+        public static async Task Method1()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("Method 1 completed successfully!");
+        }
+
+        public static async Task Method2()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("Method 2 completed successfully!");
+        }
+
+        public static async Task Method3()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("Method 3 completed successfully!");
+        }
+    }
+}
