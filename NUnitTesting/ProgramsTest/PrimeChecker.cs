c23805088e944e96bf2424cd14391ed98f1b1384
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:31:00 +0530

    Checked if the given number is prime or not

---
+++ NUnitTesting/ProgramsTest/PrimeChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class PrimeChecker
+    {
+        public bool IsPrime(int num)
+        {
+            int temp = 0;
+            for(int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    temp++;
+                }
+            }
+            if (temp > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
