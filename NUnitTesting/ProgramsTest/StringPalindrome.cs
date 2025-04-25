7f91a1539ab1c50c36ba3f8f3ecac20e8f098c24
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:31:44 +0530

    Checked if the string is palindrome or not

---
+++ NUnitTesting/ProgramsTest/StringPalindrome.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class StringPalindrome
+    {
+        public bool IsPalindrome(string str)
+        {
+            str = str.ToLower();
+            string copy = str;
+            string rev = "";
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                rev = rev + str[i];
+            }
+            if (rev == copy)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
