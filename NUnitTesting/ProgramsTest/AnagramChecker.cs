98fb44dddf1271a87b422cfe6f57671f78aab80e
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:25:25 +0530

    Checked if the two strings are anagrams of each other or not

---
+++ NUnitTesting/ProgramsTest/AnagramChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class AnagramChecker
+    {
+        public bool CheckAnagramsOrNot(string str1,string str2)
+        {
+            if (str1.Length != str2.Length)
+            {
+                return false;
+            }
+            else
+            {
+                char[] arr1 = str1.ToLower().ToCharArray();
+                char[] arr2 = str2.ToLower().ToCharArray();
+                Array.Sort(arr1);
+                Array.Sort(arr2);
+                if(new string(arr1)==new string(arr2))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
