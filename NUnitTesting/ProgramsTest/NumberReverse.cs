b8de8f570289dcb1a48da34078a35105c1539d5d
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:30:24 +0530

    Reversed the given number

---
+++ NUnitTesting/ProgramsTest/NumberReverse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class NumberReverse
+    {
+        public int ReverseNumber(int num)
+        {
+            num = Math.Abs(num);
+            int rev = 0;
+            while (num != 0)
+            {
+                int digit = num % 10;
+                rev = rev * 10 + digit;
+                num = num / 10;
+            }
+            return rev;
+        }
+    }
+}
