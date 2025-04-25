a037c9b4312b3e021f31eb40867212935cde332a
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 16:12:51 +0530

    Demonstrated the downloading of a file with a delay

---
+++ AsynchronousProgramming/AsynchronousProgramming/FileDownload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal class FileDownload
+    {
+        public static async Task<string> DownloadFileAsync()
+        {
+            Console.WriteLine("Downloading file...");
+            await Task.Delay(2000);
+            return "File downloaded!";
+        } 
+    }
+}
