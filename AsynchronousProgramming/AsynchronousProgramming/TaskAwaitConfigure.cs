d2dde8c765dec5cf5f1680782fcc5512e4351135
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 16:21:31 +0530

    Demonstrate use of ConfigureAwait(false) in async method

---
+++ AsynchronousProgramming/AsynchronousProgramming/TaskAwaitConfigure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal class TaskAwaitConfigure
+    {
+        public static async Task DoWorkAsync()
+        {
+            await Task.Delay(2000).ConfigureAwait(false);
+            Console.WriteLine("Task done without capturing context!");
+        } 
+    }
+}
