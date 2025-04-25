da34542e3e068b8e069af8c4198874f91f10b211
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:34:46 +0530

    Simulated the price change event of a stock with SMS and email alerts

---
+++ EventsAndDelegates/EventsAndDelegates/Stock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    internal class Stock
+    {
+        public event Action<decimal> PriceChanged;
+
+        public void UpdatePrice(decimal newPrice)
+        {
+            Console.WriteLine($"Stock : Price updated to {newPrice}");
+            PriceChanged?.Invoke(newPrice);
+        }
+    }
+
+    internal class EmailAlert
+    {
+        public void OnPriceChanged(decimal price)
+        {
+            Console.WriteLine($"Email Alert : New stock price is {price}");
+        }
+    }
+
+    internal class SMSAlert
+    {
+        public void OnPriceChanged(decimal price)
+        {
+            Console.WriteLine($"SMS Alert : New stock price is {price}");
+        }
+    }
+}
