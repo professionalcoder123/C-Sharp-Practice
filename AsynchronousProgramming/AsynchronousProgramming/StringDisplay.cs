33fa4483d06597dd9e0d5ecebfd25b6a6b5f7572
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 16:18:00 +0530

    Displayed the string with a delay

---
+++ AsynchronousProgramming/AsynchronousProgramming/StringDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal class StringDisplay
+    {
+        public static async Task<string> ReturnString(string str)
+        {
+            await Task.Delay(3000);
+            return "Your string is \'" + str + "\'";
+        }
+    }
+}
