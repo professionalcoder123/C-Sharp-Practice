2c14be2ced32947f659b172ef873967cf6544b95
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 16:12:09 +0530

    Demonstrated the example of fetching the data with a particular delay

---
+++ AsynchronousProgramming/AsynchronousProgramming/DataDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal class DataDisplay
+    {
+        public static async Task<string> GetDataAsync()
+        {
+            await Task.Delay(3000);
+            return "Data fetched successfully!";
+        }
+
+        public static async Task DisplayDataAsync()
+        {
+            Console.WriteLine("Fetching data...");
+            string data = await GetDataAsync();
+            Console.WriteLine(data);
+        }
+    }
+}
