3fb56131b21de397f437f67e3c5f452ca70e2592
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:23:41 +0530

    Performed the arithmetic operations using a single delegate

---
+++ EventsAndDelegates/EventsAndDelegates/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public delegate void Calculate(int num1, int num2);
+
+    internal class Calculator
+    {
+        public static void Add(int num1,int num2)
+        {
+            Console.WriteLine("Addition is " + (num1 + num2));
+        }
+
+        public static void Subtract(int num1,int num2)
+        {
+            Console.WriteLine("Subtraction is " + (num1 - num2));
+        }
+
+        public static void Multiply(int num1,int num2)
+        {
+            Console.WriteLine("Multiplication is " + (num1 * num2));
+        }
+
+        public static void Divide(int num1, int num2)
+        {
+            Console.WriteLine("Division is " + (num1 / num2));
+        }
+
+        public static void PerformCalculation(Calculate calc, int x, int y)
+        {
+            calc(x, y);
+        }
+    }
+}
