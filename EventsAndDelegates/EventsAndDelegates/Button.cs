e94c5ff0fd44c845c64569187530ec88cf7022df
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:22:17 +0530

    Simulated the click event through the button to display the message on UI

---
+++ EventsAndDelegates/EventsAndDelegates/Button.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    internal class Button
+    {
+        public event EventHandler Clicked;
+
+        public void SimulateClick()
+        {
+            Console.WriteLine("Button clicked!");
+            Clicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    internal class UIHandler
+    {
+        public void OnButtonClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("UI Handler : Button was clicked!");
+        }
+    }
+}
