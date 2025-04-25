89b95c8d6ccce8f9e01348cbdf0cd57ecda1f0a8
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 16:10:23 +0530

    Determined the difference between calling the method with and without 'async' keyword

---
+++ AsynchronousProgramming/AsynchronousProgramming/AsyncNonAsyncDifference.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal class AsyncNonAsyncDifference
+    {
+        public static async Task PrintMessageAsync()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("Message from async!");
+        }
+    }
+}
