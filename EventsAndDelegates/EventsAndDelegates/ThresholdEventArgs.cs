6fad8fa39c21b093f95f6b2c71bafb3f04aa2e95
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:36:39 +0530

    Displaying the number through event with the help of logger by specifying a condition for a number greater than 100

---
+++ EventsAndDelegates/EventsAndDelegates/ThresholdEventArgs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    internal class ThresholdEventArgs : EventArgs
+    {
+        public int Value { get; set; }
+
+        public ThresholdEventArgs(int value)
+        {
+            Value = value;
+        }
+    }
+
+    internal class ThresholdChecker
+    {
+        public event EventHandler<ThresholdEventArgs> ThresholdExceeded;
+
+        public void Check(int number)
+        {
+            if (number > 100)
+            {
+                ThresholdExceeded?.Invoke(this, new ThresholdEventArgs(number));
+            }
+            else
+            {
+                Console.WriteLine("Number is not greater than 100!");
+            }
+        }
+    }
+
+    internal class Logger
+    {
+        public void OnThresholdExceeded(object sender, ThresholdEventArgs e)
+        {
+            Console.WriteLine($"Logger : Threshold exceeded with value {e.Value}");
+        }
+    }
+}
