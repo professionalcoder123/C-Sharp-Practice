f8961824267be2db4fdce6e7b5811b2f50923888
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:26:51 +0530

    Counted the duplicate characters in the string

---
+++ NUnitTesting/ProgramsTest/DuplicateCount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class DuplicateCount
+    {
+        public int CountDuplicates(string str)
+        {
+            str = str.ToLower();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            int count = 0;
+            foreach(char ch in str)
+            {
+                if (!dict.ContainsKey(ch))
+                {
+                    dict.Add(ch, 1);
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
