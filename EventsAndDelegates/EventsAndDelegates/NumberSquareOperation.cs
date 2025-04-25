609a4839e46c5cc0ff7e9d6d3cc5363bcfc3d187
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:30:31 +0530

    Performed the square of a number through lambda expression with the help of a delegate

---
+++ EventsAndDelegates/EventsAndDelegates/NumberSquareOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public delegate int PerformSquare(int x);
+
+    internal class NumberSquareOperation
+    {
+        public static int FindSquare(int num)
+        {
+            PerformSquare square = num => num * num;
+            return square(num);
+        }
+    }
+}
