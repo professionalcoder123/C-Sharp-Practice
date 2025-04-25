8eaca0ed81da0ed08b7f4f14b189f633c88d959a
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:32:11 +0530

    Implemented the data receiving event from a publisher to subscriber

---
+++ EventsAndDelegates/EventsAndDelegates/Publisher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    internal class Publisher
+    {
+        public static event Action OnDataReceived;
+
+        public void ReceiveData()
+        {
+            Console.WriteLine("Publisher : Data received!");
+            OnDataReceived?.Invoke();
+        }
+    }
+
+    internal class Subscriber
+    {
+        public void Subscribe()
+        {
+            Publisher.OnDataReceived += HandleDataReceived;
+        }
+
+        public void HandleDataReceived()
+        {
+            Console.WriteLine("Subscriber : Data received event handled!");
+        }
+    }
+}
