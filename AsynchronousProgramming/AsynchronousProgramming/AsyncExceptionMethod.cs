eaa23b7059de8c3718fbfe487632ee34aa2c6632
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 16:07:46 +0530

    Demonstrated whether the asynchronous method should throw an exception or not depending upon the boolean variable

---
+++ AsynchronousProgramming/AsynchronousProgramming/AsyncExceptionMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal class AsyncExceptionMethod
+    {
+        public static async Task<string> MightThrowAsync(bool shouldThrow)
+        {
+            await Task.Delay(1000);
+            if (shouldThrow)
+            {
+                throw new Exception("Something went wrong!");
+            }
+            return "All good!";
+        }
+    }
+}
