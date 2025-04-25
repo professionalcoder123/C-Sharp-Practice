05c7860ea36999b4819d018addb62b8e79cdfa76
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:27:42 +0530

    Displayed the greeting messages to welcome and then again with return message with a specified delay

---
+++ EventsAndDelegates/EventsAndDelegates/GreetingMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public delegate string GreetMessage(string message);
+
+    internal class GreetingMessages
+    {
+        public static string GreetHello(string msg)
+        {
+            Console.WriteLine("Enter the name");
+            string name = Console.ReadLine();
+            string message = msg + " " + name + "!";
+            return message;
+        }
+
+        public static string GreetBye(string msg)
+        {
+            return msg;
+        }
+    }
+}
