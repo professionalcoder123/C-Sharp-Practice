8706cfc6058a2a18913addc39fd01892cf5e8e1b
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:32:43 +0530

    Reversed the given string

---
+++ NUnitTesting/ProgramsTest/StringReverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class StringReverse
+    {
+        public string ReverseString(string str)
+        {
+            str = str.ToLower();
+            string rev = "";
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                rev = rev + str[i];
+            }
+            return rev;
+        }
+    }
+}
