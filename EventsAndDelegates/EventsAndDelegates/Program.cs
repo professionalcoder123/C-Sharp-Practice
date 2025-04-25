0f8a199600c975de5a6f46451b04165c41e92517
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:37:35 +0530

    Implemented the examples of delegates and events in this class

---
+++ EventsAndDelegates/EventsAndDelegates/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace EventsAndDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyDelegate del = new MyDelegate(AdditionOperation.Add);
+            //del(2, 7);
+
+            //GreetMessage greetHello = new GreetMessage(GreetingMessages.GreetHello);
+            //Console.WriteLine(greetHello("Hello, good morning"));
+            //Thread.Sleep(2000);
+            //GreetMessage greetBye = new GreetMessage(GreetingMessages.GreetBye);
+            //Console.WriteLine(greetBye("Bye! Have a nice day!"));
+
+            //Console.WriteLine("Enter first number");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter second number");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Calculator.PerformCalculation(Calculator.Add, num1, num2);
+            //Calculator.PerformCalculation(Calculator.Subtract, num1, num2);
+            //Calculator.PerformCalculation(Calculator.Multiply, num1, num2);
+            //Calculator.PerformCalculation(Calculator.Divide, num1, num2);
+
+            //MessageDisplay.GiveMessage();
+
+            //Console.WriteLine("Enter a number");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Square of a number is " + NumberSquareOperation.FindSquare(num));
+
+            //var publisher = new Publisher();
+            //var subscriber1 = new Subscriber();
+            //var subscriber2 = new Subscriber();
+            //subscriber1.Subscribe();
+            //subscriber2.Subscribe();
+            //publisher.ReceiveData();
+
+            //Console.WriteLine("Enter a number");
+            //int num = int.Parse(Console.ReadLine());
+            //var checker = new ThresholdChecker();
+            //var logger = new Logger();
+            //checker.ThresholdExceeded += logger.OnThresholdExceeded;
+            //checker.Check(num);
+
+            //Console.WriteLine("Enter the updated price of the product");
+            //decimal price = decimal.Parse(Console.ReadLine());
+            //var stock = new Stock();
+            //var email = new EmailAlert();
+            //var sms = new SMSAlert();
+            //stock.PriceChanged += email.OnPriceChanged;
+            //stock.PriceChanged += sms.OnPriceChanged;
+            //stock.UpdatePrice(price);
+
+            //var button = new Button();
+            //var handler = new UIHandler();
+            //button.Clicked += handler.OnButtonClicked;
+            //button.SimulateClick();
+        }
+    }
+}
