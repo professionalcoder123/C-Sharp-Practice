9fe18897c0255b093c4ed0d3365de1416d05d871
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:27:27 +0530

    Checked if the number is even or odd

---
+++ NUnitTesting/ProgramsTest/EvenOddChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class EvenOddChecker
+    {
+        public string TellEvenOrOdd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return "Even number";
+            }
+            else
+            {
+                return "Odd number";
+            }
+        }
+    }
+}
