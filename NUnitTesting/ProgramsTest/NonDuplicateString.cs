ad29037f07748f7619049f53498faf097e34111d
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:29:43 +0530

    Displayed the same modified string without duplicate characters in it

---
+++ NUnitTesting/ProgramsTest/NonDuplicateString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class NonDuplicateString
+    {
+        public string ReturnNonDuplicateString(string str)
+        {
+            str = str.ToLower();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            StringBuilder newStr = new StringBuilder();
+            foreach(char ch in str)
+            {
+                if (!dict.ContainsKey(ch))
+                {
+                    dict.Add(ch, 1);
+                    newStr.Append(ch);
+                }
+            }
+            return newStr.ToString();
+        }
+    }
+}
