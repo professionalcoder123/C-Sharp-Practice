290a023a14ea36680749f5169533dc8c81303924
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:26:09 +0530

    Performed the basic arithmetic operations

---
+++ NUnitTesting/ProgramsTest/Calculations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class Calculations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
