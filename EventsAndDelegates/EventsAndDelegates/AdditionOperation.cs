444db379e7a23ba8f47412e671e142fcb875ebcb
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:20:11 +0530

    Performed the addition of two numbers using a delegate

---
+++ EventsAndDelegates/EventsAndDelegates/AdditionOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public delegate void MyDelegate(int a, int b);
+
+    internal class AdditionOperation
+    {
+        public static void Add(int x, int y)
+        {
+            Console.WriteLine("Addition is " + (x + y));
+        }
+    }
+}
