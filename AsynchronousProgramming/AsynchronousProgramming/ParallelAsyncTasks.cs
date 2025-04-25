e99740234a10f2545a41eb423d66684203d5da22
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 16:16:59 +0530

    Demonstrated the parallel execution of three tasks each with a different delay

---
+++ AsynchronousProgramming/AsynchronousProgramming/ParallelAsyncTasks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal class ParallelAsyncTasks
+    {
+        public static async Task<string> TaskA()
+        {
+            await Task.Delay(1000);
+            return "Task A done!";
+        }
+
+        public static async Task<string> TaskB()
+        {
+            await Task.Delay(2000);
+            return "Task B done!";
+        }
+
+        public static async Task<string> TaskC()
+        {
+            await Task.Delay(3000);
+            return "Task C done!";
+        }
+    }
+}
