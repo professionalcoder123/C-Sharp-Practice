a0179461a004d81bcd49ff7cba0d93383f371469
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 14:28:13 +0530

    Displayed the maximum number from two numbers

---
+++ NUnitTesting/ProgramsTest/MaximumNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramsTest
+{
+    public class MaximumNumber
+    {
+        public int FindMaximum(int num1,int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+    }
+}
