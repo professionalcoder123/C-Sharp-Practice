03bf079a045841087f90c476f464ef91b5c4c16a
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 11:29:05 +0530

    Displayed the message through the anonymous delegate

---
+++ EventsAndDelegates/EventsAndDelegates/MessageDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public delegate void DelegateMessage();
+
+    internal class MessageDisplay
+    {
+        public static void GiveMessage()
+        {
+            DelegateMessage msg = delegate ()
+            {
+                Console.WriteLine("Hello! I am inside the delegate.");
+            };
+            msg();
+        }
+    }
+}
