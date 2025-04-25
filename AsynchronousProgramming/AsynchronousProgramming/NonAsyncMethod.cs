ea51d37b68ad5ff88ee1d3bc92929f36d610e84b
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-25 16:15:29 +0530

    Implemented the method call without 'async' keyword

---
+++ AsynchronousProgramming/AsynchronousProgramming/NonAsyncMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    internal class NonAsyncMethod
+    {
+        public static Task<string> NotAsync()
+        {
+            return Task.FromResult("Hello!");
+        }
+    }
+}
