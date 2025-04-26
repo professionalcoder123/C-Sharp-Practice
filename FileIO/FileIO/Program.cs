a6a7c82eb80e1ef22c34db82ac56101af7993e08
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-26 14:22:52 +0530

    Implemented the file handling concepts in this file

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FileIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //NewFileText.WriteFile();
+
+            //FileDataReading.ReadDataFromFile();
+
+            //FileAppendText.AppendTextInFile();
+
+            //FileCopying.CopyContentsToAnotherFile();
+
+            //FileDeletion.DeleteFile();
+
+            //EmployeeCSV.StoreEmployeeInformation();
+
+            //StudentJSON.StoreStudentInformation();
+        }
+    }
+}
