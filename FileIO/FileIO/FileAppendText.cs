33e7e2a64c5ef4a4871b4381b4425345ae1bb781
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-26 14:17:32 +0530

    Added the text into a written saved file

---
+++ FileIO/FileIO/FileAppendText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    internal class FileAppendText
+    {
+        public static void AppendTextInFile()
+        {
+            string path = @"C:\Users\Aditya Turwatkar\Desktop\Aditya\New folder\C# Practice\FileIO\FileIO\MyFile.txt";
+            if (File.Exists(path))
+            {
+                File.AppendAllText(path, "\nI am using this file for implementing file handling topics.");
+                Console.WriteLine("Text appended successfully!");
+            }
+            else
+            {
+                Console.WriteLine("File does not exist!");
+            }
+        }
+    }
+}
