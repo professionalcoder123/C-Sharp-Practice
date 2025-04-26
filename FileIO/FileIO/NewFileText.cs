bc1c042cd32b245789c2b7015c915c7f05157301
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-26 14:20:51 +0530

    Created the new file in a directory and written the content in it

---
+++ FileIO/FileIO/NewFileText.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    internal class NewFileText
+    {
+        public static void WriteFile()
+        {
+            string path = @"C:\Users\Aditya Turwatkar\Desktop\Aditya\New folder\C# Practice\FileIO\FileIO\MyFile.txt";
+            using(StreamWriter writer=new StreamWriter(path))
+            {
+                writer.WriteLine("Hello! This is my new file!");
+            }
+            Console.WriteLine("File created and text written successfully!");
+        }
+    }
+}
