5286d36882ce439968df61ed67b3dbb990c71669
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-26 14:18:26 +0530

    Copied the contents from one file to another file

---
+++ FileIO/FileIO/FileCopying.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    internal class FileCopying
+    {
+        public static void CopyContentsToAnotherFile()
+        {
+            string source = @"C:\Users\Aditya Turwatkar\Desktop\Aditya\New folder\C# Practice\FileIO\FileIO\MyFile.txt";
+            string destination = @"C:\Users\Aditya Turwatkar\Desktop\Aditya\New folder\C# Practice\FileIO\FileIO\MyNewFile.txt";
+            if (File.Exists(source))
+            {
+                using(StreamReader reader=new StreamReader(source))
+                using(StreamWriter writer=new StreamWriter(destination))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+                Console.WriteLine("File copied successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Source file does not exist!");
+            }
+        }
+    }
+}
