b0f8bdd20b254a13c71015ab1712ae69f87f1a67
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-26 14:19:58 +0530

    Deleted the file existing from a path

---
+++ FileIO/FileIO/FileDeletion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    internal class FileDeletion
+    {
+        public static void DeleteFile()
+        {
+            string path = @"C:\Users\Aditya Turwatkar\Desktop\Aditya\New folder\C# Practice\FileIO\FileIO\MyFile.txt";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                Console.WriteLine("File deleted successfully!");
+            }
+            else
+            {
+                Console.WriteLine("File does not exist!");
+            }
+        }
+    }
+}
