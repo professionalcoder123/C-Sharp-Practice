aa3a8ca94c6859caad71cd42990f4f90f355c67d
Aditya Turwatkar <aditya.turwatkar@gmail.com>
2025-04-26 14:19:18 +0530

    Read the written data from a file

---
+++ FileIO/FileIO/FileDataReading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    internal class FileDataReading
+    {
+        public static void ReadDataFromFile()
+        {
+            string path = @"C:\Users\Aditya Turwatkar\Desktop\Aditya\New folder\C# Practice\FileIO\FileIO\MyFile.txt";
+            if (File.Exists(path))
+            {
+                string content = File.ReadAllText(path);
+                Console.WriteLine("File content :");
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine("File does not exist!");
+            }
+        }
+    }
+}
